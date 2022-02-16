e885151b74fff0e25f4d4ba638758107f744a314
gsemir0418 <845217811@qq.com>
2022-02-16 09:58:32 +0800

    完成注册页面及表单校验

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
 import {
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -8,6 +9,9 @@ import {
 } from "typeorm";
 import { Post } from "./Post";
 import { Comment } from "./Comment";
+import { getDatabaseConnection } from "lib/getDatabaseConnection";
+import md5 from "md5";
+import _ from "lodash";
 
 @Entity("users")
 export class User {
@@ -27,4 +31,60 @@ export class User {
   // 用户与评论的一对多关系
   @OneToMany((type) => Comment, (comment) => comment.user)
   comments: Comment[];
+  // 以上为users表的列配置
+
+  // 以下为实例属性与方法
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirm: [] as string[],
+  };
+  // 避免与passwordDigest混淆
+  password: string;
+  passwordConfirm: string;
+  // 校验方法，涉及到查询数据库，因此是异步的
+  async validate() {
+    if (this.username.trim() === "") {
+      this.errors.username.push("不能为空");
+    }
+    if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+      this.errors.username.push("格式不合法");
+    }
+    if (this.username.trim().length > 42) {
+      this.errors.username.push("太长");
+    }
+    if (this.username.trim().length <= 3) {
+      this.errors.username.push("太短");
+    }
+    if (this.password === "") {
+      this.errors.password.push("不能为空");
+    }
+    if (this.password !== this.passwordConfirm) {
+      this.errors.passwordConfirm.push("密码不匹配");
+    }
+    const hasUser = await (
+      await getDatabaseConnection()
+    ).manager.find(User, { username: this.username });
+    if (hasUser.length > 0) {
+      this.errors.username.push("用户名已存在");
+    }
+  }
+  // 是否有错误
+  hasErrors() {
+    return !!Object.values(this.errors).flat(Infinity).length;
+  }
+  // 在向数据库存入数据前，处理密码加密
+  @BeforeInsert()
+  generatePasswordDigest() {
+    this.passwordDigest = md5(this.password);
+  }
+  // 类中如果有toJSON方法，其返回值则代表此类的实例stringify后的值
+  toJSON() {
+    return _.omit(this, [
+      "password",
+      "passwordConfirm",
+      "passwordDigest",
+      "errors",
+    ]);
+  }
 }
