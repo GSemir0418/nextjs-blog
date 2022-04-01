acd8094a0c6bbafd34b0ff114cde56007d6567ba
GSemir <845217811@qq.com>
2022-04-01 16:50:19 +0800

    feat: 新增按钮与编辑按钮

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,10 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import { getDatabaseConnection } from "lib/getDatabaseConnection";
 import { Post } from "src/entity/Post";
 import Link from "next/link";
 import qs from "query-string";
 import { usePager } from "hooks/usePager";
+import withSession from "lib/withSession";
 
 type Props = {
   posts: Post[];
@@ -11,15 +12,23 @@ type Props = {
   page: number;
   pageSize: number;
   totalPage: number;
+  currentUser: User;
 };
 
 const PostsIndex: NextPage<Props> = (props) => {
-  const { posts, count, page, totalPage } = props;
+  const { currentUser, posts, count, page, totalPage } = props;
   const { pager } = usePager({ page, totalPage });
   return (
     <>
       <div className="posts">
-        <h1>文章列表 ({count})</h1>
+        <header>
+          <h1>文章列表 ({count})</h1>
+          {currentUser && (
+            <Link href={"/posts/new"}>
+              <a>添加文章</a>
+            </Link>
+          )}
+        </header>
         {posts.map((post) => (
           <div className="onePost">
             <Link key={post.id} href={`/posts/${post.id}`}>
@@ -35,6 +44,13 @@ const PostsIndex: NextPage<Props> = (props) => {
           margin: 0 auto;
           padding: 16px;
         }
+        .posts > header {
+          display: flex;
+          align-items: center;
+        }
+        .posts > header > h1 {
+          margin-right: auto;
+        }
         .onePost {
           border-bottom: 1px solid #ddd;
           padding: 8px 0;
@@ -45,26 +61,31 @@ const PostsIndex: NextPage<Props> = (props) => {
 };
 export default PostsIndex;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await getDatabaseConnection();
-  // 分页
-  // 从url中分离出page
-  const { query } = qs.parseUrl(context.req.url);
-  const page = parseInt(query.page?.toString() || "1");
-  const pageSize = 5;
-  // 查询数据库，并返回posts和总数count
-  const [posts, count] = await connection.manager.findAndCount(Post, {
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-  });
-  return {
-    props: {
-      // 将数据返回给前端SSR页面
-      posts: JSON.parse(JSON.stringify(posts)),
-      count,
-      page,
-      pageSize,
-      totalPage: Math.ceil(count / pageSize),
-    },
-  };
-};
+export const getServerSideProps: GetServerSideProps = withSession(
+  async (context:GetServerSidePropsContext) => {
+    const connection = await getDatabaseConnection();
+    // 后端从session中取出currentUser
+    const currentUser = (context.req as any).session.get("currentUser") || null;
+    // 分页
+    // 从url中分离出page
+    const { query } = qs.parseUrl(context.req.url);
+    const page = parseInt(query.page?.toString() || "1");
+    const pageSize = 5;
+    // 查询数据库，并返回posts和总数count
+    const [posts, count] = await connection.manager.findAndCount(Post, {
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    });
+    return {
+      props: {
+        currentUser,
+        // 将数据返回给前端SSR页面
+        posts: JSON.parse(JSON.stringify(posts)),
+        count,
+        page,
+        pageSize,
+        totalPage: Math.ceil(count / pageSize),
+      },
+    };
+  }
+);
